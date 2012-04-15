3e94b29eed7f53b50e5f83a82137d93dbc9dc200
David Gil <dgilperez@gmail.com>
2012-04-16 00:34:26 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusinessModelCanvas::Application.config.secret_token = 'f3cb421b4421d80996654e10e8db9d2047444ac0dec4eca810b526371d3be2a2cd579644668d5978175dd7c512131041e02f80e0c77d0ce3b49766cd71ad5363'
